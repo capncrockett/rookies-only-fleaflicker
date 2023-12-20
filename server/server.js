3994145ccc1d6eeb539356cecf6f1de5dd92cebf
Joe Davey <joe.davey@elevenfiftynine.com>
2023-12-20 15:26:23 -0800

    add API calls and constants

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
-const express = require("express");
-const axios = require("axios");
-const cors = require("cors");
+import express from "express";
+import axios from "axios";
+import cors from "cors";
+import {} from "constants";
 
 const app = express();
 app.use(cors()); // Enable CORS
@@ -13,7 +14,84 @@ app.get("/api/league-rosters", (req, res) => {
         sport: req.query.sport,
         league_id: req.query.league_id,
         season: req.query.season,
-        // Include any other parameters you need
+        scoring_period: req.query.scoring_period,
+        external_id_type: req.query.external_id_type,
+      },
+    })
+    .then((apiResponse) => {
+      res.send(apiResponse.data);
+    })
+    .catch((error) => {
+      res
+        .status(500)
+        .send("Error fetching from Fleaflicker API: " + error.message);
+    });
+});
+
+app.get("/api/FetchRoster", (req, res) => {
+  axios
+    .get("https://www.fleaflicker.com/api/FetchRoster", {
+      params: {
+        sport: req.query.sport,
+        league_id: req.query.league_id,
+        team_id: req.query.team_id,
+        season: req.query.season,
+        scoring_period: req.query.scoring_period,
+        external_id_type: req.query.external_id_type,
+      },
+    })
+    .then((apiResponse) => {
+      res.send(apiResponse.data);
+    })
+    .catch((error) => {
+      res
+        .status(500)
+        .send("Error fetching from Fleaflicker API: " + error.message);
+    });
+});
+app.get("/api/FetchPlayerListing", (req, res) => {
+  axios
+    .get("https://www.fleaflicker.com/api/FetchPlayerListing", {
+      params: {
+        sport: req.query.sport,
+        league_id: req.query.league_id,
+        sort: req.query.sort,
+        "filter.position.label": req.query["filter.position.label"],
+        "filter.position.group": req.query["filter.position.group"],
+        "filter.position.eligibility": req.query["filter.position.eligibility"],
+        "filter.position.min": req.query["filter.position.min"],
+        "filter.position.max": req.query["filter.position.max"],
+        "filter.position.start": req.query["filter.position.start"],
+        "filter.position.colors": req.query["filter.position.colors"],
+        "filter.position_eligibility": req.query["filter.position_eligibility"],
+        "filter.free_agent_only": req.query["filter.free_agent_only"],
+        "filter.experience": req.query["filter.experience"],
+        "filter.mlb_upcoming_pitchers_only":
+          req.query["filter.mlb_upcoming_pitchers_only"],
+        "filter.player_id": req.query["filter.player_id"],
+        "filter.query": req.query["filter.query"],
+        result_offset: req.query.result_offset,
+        sort_season: req.query.sort_season,
+        sort_period: req.query.sort_period,
+        external_id_type: req.query.external_id_type,
+      },
+    })
+    .then((apiResponse) => {
+      res.send(apiResponse.data);
+    })
+    .catch((error) => {
+      res
+        .status(500)
+        .send("Error fetching from Fleaflicker API: " + error.message);
+    });
+});
+
+app.get("/api/FetchLeagueRules", (req, res) => {
+  axios
+    .get("https://www.fleaflicker.com/api/FetchLeagueRules", {
+      params: {
+        sport: req.query.sport,
+        league_id: req.query.league_id,
       },
     })
     .then((apiResponse) => {
--- src/App.js
+++ src/App.js
@@ -1,36 +1,45 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
-// import React, { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
-  console.log("Hi");
+  const [rosters, setRosters] = useState(null);
 
-  const rosters = axios
-    .get("http://localhost:3001/api/league-rosters", {
-      params: {
-        sport: "NFL",
-        league_id: 343168,
-        season: 2023,
-        // Add any other parameters
-      },
-    })
-    .then((response) => {
-      console.log(response.data); // Handle the response data
-    })
-    .catch((error) => {
-      console.error("There was an error!", error);
-    });
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/api/league-rosters", {
+        params: {
+          sport: "NFL",
+          league_id: 343168,
+          season: 2023,
+          // Add any other parameters
+        },
+      })
+      .then((response) => {
+        console.log(response.data); // Handle the response data
+        setRosters(response.data); // Update state with the response data
+      })
+      .catch((error) => {
+        console.error("There was an error!", error);
+      });
+  }, []); // Empty dependency array means this effect runs once on mount
 
+  // Log the rosters data (this will initially log null until the data is fetched)
   console.table(rosters);
+  const teamRostersArray = rosters.rosters;
+  console.log(teamRostersArray[0]);
+  const firstRoster = teamRostersArray[0];
+  const secondRoster = teamRostersArray[1];
+  const thirdRoster = teamRostersArray[2];
+  const fourthRoster = teamRostersArray[3];
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>{firstRoster.team.name}</p>
+        <p>{secondRoster.team.name}</p>
+        <p>{thirdRoster.team.name}</p>
+        <p>{fourthRoster.team.name}</p>
       </header>
     </div>
   );
