41977c8cea3a349d1ddc39723011949411c99329
Joe Davey <joe.davey@elevenfiftynine.com>
2023-12-22 09:42:06 -0800

    update JS imports

--- server/server.js
+++ server/server.js
@@ -2,12 +2,13 @@
 import express from "express";
 import axios from "axios";
 import cors from "cors";
-import config from "../config"; // Adjust the path as needed
+import config from "../config.js"; // Adjust the path as needed
 
 const app = express();
 app.use(cors()); // Enable CORS
 
 app.get("/api/league-rosters", (req, res) => {
+  console.log("req.query", req.query); // Log the request query
   axios
     .get(`${config.apiBaseUrl}${config.endpoints.leagueRosters}`, {
       params: {
@@ -19,32 +20,16 @@ app.get("/api/league-rosters", (req, res) => {
       res.send(apiResponse.data);
     })
     .catch((error) => {
+      console.error(error); // Log the full error
       res
         .status(500)
         .send("Error fetching from Fleaflicker API: " + error.message);
     });
 });
 
-app.get("/api/roster", (req, res) => {
-  axios
-    .get(`${config.apiBaseUrl}${config.endpoints.roster}`, {
-      params: {
-        ...config.commonParams,
-        team_id: req.query.team_id, // This is specific to the roster endpoint
-        ...req.query,
-      },
-    })
-    .then((apiResponse) => {
-      res.send(apiResponse.data);
-    })
-    .catch((error) => {
-      res
-        .status(500)
-        .send("Error fetching from Fleaflicker API: " + error.message);
-    });
-});
-
-// The rest of your endpoints would follow the same pattern...
+// ...rest of your server.js code
 
 const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`); // Confirm server is running
+});
--- src/App.jsx
+++ src/App.jsx
@@ -18,30 +18,33 @@ function App() {
         },
       })
       .then((response) => {
-        console.log(response.data); // Handle the response data
-        setRosters(response.data); // Update state with the response data
+        setRosters(response.data.rosters || []); // Update state with the response data
+        setLoading(false);
       })
       .catch((error) => {
         console.error("There was an error!", error);
+        setError(error);
+        setLoading(false);
       });
   }, []); // Empty dependency array means this effect runs once on mount
 
-  // Log the rosters data (this will initially log null until the data is fetched)
-  console.table(rosters);
-  const teamRostersArray = rosters.rosters;
-  console.log(teamRostersArray[0]);
-  const firstRoster = teamRostersArray[0];
-  const secondRoster = teamRostersArray[1];
-  const thirdRoster = teamRostersArray[2];
-  const fourthRoster = teamRostersArray[3];
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error fetching rosters: {error.message}</div>;
+  }
+
+  const [firstRoster, secondRoster, thirdRoster, fourthRoster] = rosters;
 
   return (
     <div className="App">
       <header className="App-header">
-        <p>{firstRoster.team.name}</p>
-        <p>{secondRoster.team.name}</p>
-        <p>{thirdRoster.team.name}</p>
-        <p>{fourthRoster.team.name}</p>
+        <p>{firstRoster?.team.name}</p>
+        <p>{secondRoster?.team.name}</p>
+        <p>{thirdRoster?.team.name}</p>
+        <p>{fourthRoster?.team.name}</p>
       </header>
     </div>
   );
